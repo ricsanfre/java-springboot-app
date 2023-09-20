c34b8e3b212ee0c164b317e629669750d1518591
Ricardo Sanchez <ricsanfre@gmail.com>
2023-09-20 21:05:09 +0200

    Adding Customers to DB on start-up of App

--- src/main/java/com/ricsanfre/Main.java
+++ src/main/java/com/ricsanfre/Main.java
@@ -1,7 +1,13 @@
 package com.ricsanfre;
 
+import com.ricsanfre.customer.Customer;
+import com.ricsanfre.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -9,6 +15,16 @@ public class Main {
     public static void main(String[] args) {
 
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository){
+        return args -> {
+            Customer alex = new Customer("alex","alex@mail.com", 21);
+            Customer juan = new Customer("juan","juan@mail.com", 27);
+            List<Customer> customers = List.of(alex, juan);
+            customerRepository.saveAll(customers);
+        };
     }
 
 
