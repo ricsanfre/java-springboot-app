f59f0c27a61fcda930495e057af66a46917fd889
Ricardo Sanchez <ricsanfre@gmail.com>
2023-10-12 15:58:15 +0200

    Adding Slack messages to CD workflow

--- backend/src/main/java/com/ricsanfre/demo/customer/CustomerConfiguration.java
+++ backend/src/main/java/com/ricsanfre/demo/customer/CustomerConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 import java.util.Random;
 
 @Configuration
