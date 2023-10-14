db8c5f62075ddce71b56d5c38fff77b2e9f7248f
Ricardo Sanchez <ricsanfre@gmail.com>
2023-10-14 11:57:32 +0200

    Adding React front-end scaffolding. Vite JS + Chakra UI

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+// Main App
+const App = () => {
+    return (
+        <Button colorScheme='teal' variant='solid'>Click Me</Button>
+    )
+}
+
+export default App
